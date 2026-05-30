9a9afe4567d569fd99e9aa146b5e28130638ecc3
Abril <adebiasi@uade.edu.ar>
2026-05-30 18:57:08 -0300

    Implementacion de clases Corredor y Carrera

--- carreracaballos/src/main/java/model/Carrera.java
+++ carreracaballos/src/main/java/model/Carrera.java
@@ -1,8 +1,142 @@
 package model;
 
-public class Carrera {
-	private Pista pista;
-	private boolean finalizada;
-	
+import java.util.ArrayList;
+import java.util.List;
 
+public class Carrera {
+
+	private Pista pista;
+	private List<Corredor> corredores;
+	private Jugador jugador;
+	private boolean finalizada;
+	private Corredor ganador;
+	private Corredor segundo;
+	private List<Corredor> corredoresFinalizados;
+
+	public Carrera(Pista pista, List<Corredor> corredores, Jugador jugador) {
+		this.pista = pista;
+		this.corredores = new ArrayList<Corredor>();
+		this.corredoresFinalizados = new ArrayList<Corredor>();
+		this.jugador = jugador;
+		this.finalizada = false;
+		this.ganador = null;
+		this.segundo = null;
+		
+		prepararCorredores(corredores);
+	}
+
+	public void prepararCorredores(List<Corredor> corredores) {
+		this.corredores.clear();
+		this.corredoresFinalizados.clear();
+
+		for (Corredor corredor : corredores) {
+			corredor.reiniciarEstado();
+			this.corredores.add(corredor);
+		}
+
+		this.finalizada = false;
+		this.ganador = null;
+		this.segundo = null;
+	}
+
+	public void simularTurno() {
+		if (this.finalizada) {
+			return;
+		}
+
+		for (Corredor corredor : corredores) {
+			if (!corredoresFinalizados.contains(corredor)) {
+
+				double distanciaRestante = pista.getDistancia() - corredor.getDistanciaRecorrida();
+
+				if (distanciaRestante < 0) {
+					distanciaRestante = 0;
+				}
+
+				corredor.avanzar(distanciaRestante);
+
+				if (corredor.llegaAMeta(pista.getDistancia())) {
+					registrarLlegada(corredor);
+				}
+			}
+		}
+
+		verificarFinalizacion();
+	}
+
+	public void registrarLlegada(Corredor corredor) {
+		if (!corredoresFinalizados.contains(corredor)) {
+			corredoresFinalizados.add(corredor);
+		}
+	}
+
+	public void verificarFinalizacion() {
+		if (!corredoresFinalizados.isEmpty()) {
+			asignarGanadorYSegundo();
+			this.finalizada = true;
+			
+			// TODO: cuando exista CalculadorPuntaje o CarreraController,
+			// se debería asignar el puntaje del jugador desde ahí.
+		}
+	}
+
+	public int obtenerPosicion(Corredor corredor) {
+		int posicion = 1;
+
+		for (Corredor otroCorredor : corredores) {
+			if (otroCorredor != corredor &&
+				otroCorredor.getDistanciaRecorrida() > corredor.getDistanciaRecorrida()) {
+				posicion++;
+			}
+		}
+
+		return posicion;
+	}
+
+	public boolean estaFinalizada() {
+		return finalizada;
+	}
+
+	public Corredor getGanador() {
+		return ganador;
+	}
+
+	public Corredor getSegundo() {
+		return segundo;
+	}
+
+	public void asignarGanadorYSegundo() {
+		this.ganador = null;
+		this.segundo = null;
+
+		for (Corredor corredor : corredores) {
+			if (ganador == null ||
+				corredor.getDistanciaRecorrida() > ganador.getDistanciaRecorrida()) {
+				
+				segundo = ganador;
+				ganador = corredor;
+				
+			} else if (segundo == null ||
+					   corredor.getDistanciaRecorrida() > segundo.getDistanciaRecorrida()) {
+				
+				segundo = corredor;
+			}
+		}
+	}
+
+	public Pista getPista() {
+		return pista;
+	}
+
+	public List<Corredor> getCorredores() {
+		return corredores;
+	}
+
+	public Jugador getJugador() {
+		return jugador;
+	}
+
+	public List<Corredor> getCorredoresFinalizados() {
+		return corredoresFinalizados;
+	}
 }
--- carreracaballos/src/main/java/model/Corredor.java
+++ carreracaballos/src/main/java/model/Corredor.java
@@ -4,11 +4,80 @@ public class Corredor {
 	
 	private double distanciaRecorrida;
 	private String nombre;
-	private double velociad;
-	private double energia; 
-	private double resistencia; 
+	private double velocidadBase;
+	private double energiaActual; 
+	private double resistencia;
+
+	// Cuando exista la clase EstrategiaAvance, se puede agregar:
+	// private EstrategiaAvance estrategiaAvance;
 	
-	public void Corredor () {
-		int a = 0;
+	public Corredor() {
+		this.nombre = "";
+		this.velocidadBase = 0;
+		this.resistencia = 0;
+		this.energiaActual = 0;
+		this.distanciaRecorrida = 0;
+	}
+	
+	public Corredor(String nombre, double velocidadBase, double resistencia) {
+		this.nombre = nombre;
+		this.velocidadBase = velocidadBase;
+		this.resistencia = resistencia;
+		this.energiaActual = resistencia;
+		this.distanciaRecorrida = 0;
+	}
+	
+	public void avanzar(double distanciaRestante) {
+		double metrosAvanzados = velocidadBase;
+		
+		if (energiaActual <= 0) {
+			metrosAvanzados = velocidadBase / 2;
+		}
+		
+		if (metrosAvanzados > distanciaRestante) {
+			metrosAvanzados = distanciaRestante;
+		}
+		
+		double gastoEnergia = 1;
+		
+		actualizarDistanciaYEnergia(metrosAvanzados, gastoEnergia);
+	}
+	
+	public void reiniciarEstado() {
+		this.distanciaRecorrida = 0;
+		this.energiaActual = resistencia;
+	}
+	
+	public boolean llegaAMeta(double distanciaTotal) {
+		return distanciaRecorrida >= distanciaTotal;
+	}
+	
+	public void actualizarDistanciaYEnergia(double metros, double gasto) {
+		this.distanciaRecorrida = this.distanciaRecorrida + metros;
+		this.energiaActual = this.energiaActual - gasto;
+		
+		if (this.energiaActual < 0) {
+			this.energiaActual = 0;
+		}
+	}
+
+	public double getDistanciaRecorrida() {
+		return distanciaRecorrida;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public double getEnergiaActual() {
+		return energiaActual;
+	}
+
+	public double getVelocidadBase() {
+		return velocidadBase;
+	}
+
+	public double getResistencia() {
+		return resistencia;
 	}
 }
