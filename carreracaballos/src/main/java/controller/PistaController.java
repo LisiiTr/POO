986ae1e836e863636ef9ea783e39bfbc8aef8474
Lautaro <lautaroe.umerez@gmail.com>
2026-05-30 21:19:58 -0300

    jugador y pista controller

--- carreracaballos/src/main/java/controller/PistaController.java
+++ carreracaballos/src/main/java/controller/PistaController.java
@@ -1,5 +1,21 @@
 package controller;
-
+import model.Pista;
 public class PistaController {
 
-}
+    private Pista pista;
+
+    public PistaController(Pista pista) {
+        this.pista = pista;
+    }
+
+    public String getNombrePista() {
+        return pista.getNombre();
+    }
+
+    public double getDistanciaTotal() {
+        return pista.getDistancia();
+    }
+}	
+	
+
+
